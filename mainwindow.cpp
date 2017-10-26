f2a0c1c11893c22a0ca0f1089685bdab28455948
kimphg <pkp013@gmail.com>
2017-10-26 23:26:17 +0700

    lll

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,16 +14,22 @@ MainWindow::MainWindow(QWidget *parent) :
         printf((char*)port.portName().toStdString().data());
 
     }
-    QString portname = (portList.last()).portName();
-    if(!openSerial(portname,9600))
+    if(portList.size())
     {
-        printf("\nSerial port fail");
-    }
-    else
-    {
-        printf("\nSerial port active:");
-        printf(portname.toStdString().data());
-        connect(&serialPort, SIGNAL(readyRead()), this, SLOT(ProcessSerialData()));
+        QString portname = (portList.last()).portName();
+        if(!openSerial(portname,9600))
+        {
+            printf("\nSerial port fail");
+        }
+        else
+        {
+            serialPort.write("$PC100");
+            printf("\nSerial port active:");
+            printf(portname.toStdString().data());
+            connect(&serialPort, SIGNAL(readyRead()), this, SLOT(ProcessSerialData()));
+            serialPort.write("$PC100");
+
+        }
     }
 }
 
@@ -36,6 +42,28 @@ void MainWindow::ProcessSerialData()
     if(serialPort.isOpen())
     {
         serialData.append(serialPort.readAll());
+        QStringList listMsg = serialData.split("$");
+        foreach ( QString str, listMsg) {
+            if(str.left(4) == "KTTB")
+            {
+                if(str.size()<13)continue;
+                QString substr = str.right(9);
+                if(substr=="100000000")
+                {
+
+                }
+                else if(substr=="100000000")
+                {
+                    1. Nguồn +5V
+                    2. nguồn +12.6V
+                    3. Nguồn + 20V
+                    4. Nguồn +27V
+
+                    5. NGuồn : - 12.6V
+                    6. Nguồn  : - 20V
+                }
+            }
+        }
     }
 }
 bool MainWindow::openSerial(const QString &port, qint32 baudRate)
